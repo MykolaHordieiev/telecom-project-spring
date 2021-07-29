6f51b65b18a56e3162e0acb47939febe57348e30
Mykola Hordieiev <gordeev1919@gmail.com>
2021-07-29 11:20:30 +0300

    added enableWebMVC

--- src/main/java/org/project/spring/telecom/Application.java
+++ src/main/java/org/project/spring/telecom/Application.java
@@ -1,22 +1,22 @@
 package org.project.spring.telecom;
 
-import org.project.spring.telecom.infra.db.LiquibaseStarter;
-import org.project.spring.telecom.infra.web.*;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.ServletException;
+import java.io.File;
+
 @Configuration
-@ComponentScan("org.project.spring.telecom")
+@ComponentScan(basePackages = "org.project.spring.telecom")
 public class Application {
 
-    public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(Application.class);
-
-        //web
-        ServerStarter serverStarter = applicationContext.getBean(ServerStarter.class);
-        serverStarter.startServer();
+    public static void main(String[] args) throws ServletException, LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.addWebapp("/telecom", new File("webapp").getAbsolutePath());
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
