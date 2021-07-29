90dc33c8f388d545edd48d917d6ffd916fddcc85
Mykola Hordieiev <gordeev1919@gmail.com>
2021-07-29 12:13:06 +0300

    delete ServerStarter and ServerConfig

--- src/main/java/org/project/spring/telecom/infra/web/tag/LanguageTag.java
+++ src/main/java/org/project/spring/telecom/infra/web/tag/LanguageTag.java
@@ -1,7 +1,6 @@
 package org.project.spring.telecom.infra.web.tag;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 
 import javax.servlet.http.HttpSession;
 import javax.servlet.jsp.tagext.TagSupport;
@@ -9,12 +8,11 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+@Log4j2
 public class LanguageTag extends TagSupport {
 
-    private static Logger log = LogManager.getLogger(LanguageTag.class);
-
     private String message;
-    private Utf8Control utf8Control = new Utf8Control();
+    private final Utf8Control utf8Control = new Utf8Control();
 
     public void setMessage(String message) {
         this.message = message;
